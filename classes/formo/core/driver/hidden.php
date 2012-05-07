f654a51ffd0b2101ee441b6cec0a4bf4859d5dc4
CraigCamm <craigcammarata@mongoosemetrics.com>
2012-05-07 09:36:23 -0400

    Add 'id' value to hidden field

--- classes/formo/core/driver/hidden.php
+++ classes/formo/core/driver/hidden.php
@@ -11,11 +11,13 @@ class Formo_Core_Driver_Hidden extends Formo_Driver {
 	protected $_view_file = 'hidden';
 
 	public function html()
-	{
+    {
+    
 		$this->_view
 			->set_var('tag', 'input')
 			->attr('type', 'hidden')
 			->attr('name', $this->name())
+			->attr('id', $this->name())
 			->attr('value', $this->_field->val());
 	}
 
